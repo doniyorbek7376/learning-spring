66883960b5d2de2902e477903795814e5378e2ab
doniyorbek7376 <doniyorbek6439@gmail.com>
2021-01-31 11:01:55 +0500

    Get request with parameters

--- src/main/java/uz/doniyorbek7376/vegshop/controllers/FirstController.kt
+++ src/main/java/uz/doniyorbek7376/vegshop/controllers/FirstController.kt
@@ -2,12 +2,15 @@ package uz.doniyorbek7376.vegshop.controllers
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import javax.servlet.http.HttpServletRequest
 
 @Controller
 class FirstController {
 
     @GetMapping("/hello")
-    fun helloPage(): String {
+    fun helloPage(@RequestParam("name") name: String): String {
+        println(name)
         return "first/hello"
     }
 
